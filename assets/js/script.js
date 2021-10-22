df65f81dd2215120b123ad342be68bfcefcf63e6
Mbaku Starcrush <j.c.nolan9@gmail.com>
2021-10-21 22:00:23 -0700

    Making sure the weather data can be saved to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ let fetchURL;
 var date = document.querySelector("date")
 var startLoc = document.querySelector("start-loc")
 var endLoc = document.querySelector("end-loc")
-var submit = docuement.querySelector("submit")
+var submit = document.querySelector("submit")
 
 var pastSearchArray = []
 var weatherData = []
@@ -49,38 +49,66 @@ function fetchFunc(x) {
                     forecastDataObj.weatherIcon = data.list[index].weather[0].icon
                     weatherData.push(forecastDataObj);
                 }
-                console.log(forecastData);
-                for (let index = 0; index < forecastData.length; index++) {
+                //console.log(weatherData);
+                for (let index = 0; index < weatherData.length; index++) {
                     weatherData[index].date = convertedDate = (dayjs.unix(weatherData[index].date)).format("MMM DD, YYYY");
                     weatherData[index].weatherIcon = "http://openweathermap.org/img/wn/" + weatherData[index].weatherIcon + "@2x.png"
                 }
                 console.log(weatherData)
                 findDate(sampleDate)
+
+                findWeatherAddLocalStorage()
             }
+
         })
+
 };
 
 //finds the info for date that is input 
 function findDate(x) {
 
-    let sampleDate = dayjs(x).format("MMM DD, YYYY");
-    var indexValue = forecastData.find(function (post, index) {
-        if (post.date == sampleDate)
+    let weatherDate = dayjs(x).format("MMM DD, YYYY");
+    var indexValue = weatherData.find(function (post, index) {
+        if (post.date == weatherDate)
             return true;
     });
     console.log(indexValue);
 }
 
+/*
+find the weather object with same date the user input in the text field and 
+return the weather object with the matching date
+*/  
+function findWeatherAddLocalStorage() {
+    
+    console.log(weatherData)
+    
+    for (var i = 0; i < weatherData.length; i++) {
+        console.log(weatherData[i])
+        console.log(weatherData[i].date)
+        console.log(sampleDate)
+        if (weatherData[i].date == dayjs(sampleDate).format("MMM DD, YYYY")) {
+            
+            weather = weatherData[i]
+        }
+    }
+    console.log(weather)
+    pastSearchArray.push(weather)
+    console.log(pastSearchArray)
+    var pastString = JSON.stringify(pastSearchArray)
+    localSearchHistory = localStorage.setItem("history", pastString)
+}
+
 var localSearchHistory = JSON.parse(localStorage.getItem("history"))
 
-//if a user comes to the page for the first time local storage is empty
-//set the date, starting location, and ending location to empty strings 
+// if a user comes to the page for the first time local storage is empty
+// set the date, starting location, and ending location to empty strings 
 if (localSearchHistory == null) {
     pastSearchArray = []
-    localSearchHistory = pastSearchArray
-    date.textContent = ""
-    startLoc.textContent = ""
-    endLoc.textContent = ""
+    localSearchHistory = JSON.parse(pastSearchArray)
+    // date.textContent = ""
+    // startLoc.textContent = ""
+    // endLoc.textContent = ""
 }
 //if local storage is not null aka empty then the user has been on the page before
 //Load their previous information into the "pastSearchArray"
@@ -95,26 +123,20 @@ submit.addEventListener("click", function () {
     pastSearchArray.push({ startLoc: startLoc.textContent })
     pastSearchArray.push({ endLoc: endLoc.textContent })
 
-    //fetch weather
-
-    fetchCity("Sad");
+    //fetch weather data
+    fetchCity(startLoc.textContent) 
 
     //fetch flight data 
 
-    //find the weather object with same date the user input in the text field and return the weather object with the matching date 
-    for (var i = 0; i < weatherData.length; i++) {
-        if (weatherData[i].date == date) {
-            weather = weatherData[i]
-        }
-    }
-    pastSearchArray.push(weather)
-
-    //do something with the flight data 
+    //do something with the flight data
+    
     pastSearchArray.push(flightCarrierPrice)
 
     localSearchHistory = JSON.stringify(localStorage.setItem("history", pastSearchArray))
 
 })
+
+// fetchCity("Chicago")
 
 
 
