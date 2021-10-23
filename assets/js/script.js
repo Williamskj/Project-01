759ec8125e83841696f0cd636e71f78ebc3a2c9d
Keyann <wkeyann@gmail.com>
2021-10-23 13:13:45 -0700

    Initial Upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
-$('#search-button').on('click', function (event) {
-    console.log("js script connected")
-    fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/?query=Seattle", {
+var city1 = ''
+var city2 = ''
+function startingCity() {
+    fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/?query=" + $('#startingCity').val(), {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
@@ -9,21 +10,57 @@ $('#search-button').on('click', function (event) {
     })
         .then(function (response) {
             return response.json()
-        }).then(function (data) {
-            console.log('data', data.Places)
+        }).then(function (data1) {
+            console.log('data1', data1.Places[0].CityId)
+            city1 = data1.Places[0].CityId
+            endingCity()
+        })
 
+}
+
+function endingCity() {
+    fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/?query=" + $('#endingCity').val(), {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+            "x-rapidapi-key": "ed8a1977c6mshff09b4ba3a0ad2ep1ae63bjsnaaca95db666c"
+        }
+    })
+        .then(function (response) {
+            return response.json()
+        }).then(function (data2) {
+            console.log('data2', data2.Places[0].CityId)
+            city2 = data2.Places[0].CityId
+            quote()
+        })
+}
+
+function quote() {
+    fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/" + city1 + "/" + city2 + "/" + $('#date').val(), {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+            "x-rapidapi-key": "ed8a1977c6mshff09b4ba3a0ad2ep1ae63bjsnaaca95db666c"
+        }
+    })
+        .then(function (response) {
+            return response.json()
+        }).then(function (results) {
             $('#api-container').empty()
-            for (let i = 0; i < data.Places.length; i++) {
-                var h1 = $('<h1>')
-                var p = $('<p>')
-                h1.text(data.Places[i].PlaceName)
-                p.text(data.Places[i].RegionId)
-
-                $('#api-container').append(h1, p)
+            for (let i = 0; i < results.Places.length; i++) {
+                console.log('Places', results.Places[i])
+            } for (let i = 0; i < results.Carriers.length; i++) {
+                console.log('Carriers', results.Carriers[i])
+            } for (let i = 0; i < results.Quotes.length; i++) {
+                console.log('Quotes', results.Quotes[i])
             }
         })
         .catch(err => {
             console.error(err);
         });
+}
+
+$('#sudmit').on('click', function (event) {
+    startingCity()
     event.preventDefault()
-})
+});
